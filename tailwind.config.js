93232476e42369b8486caa40ebe37f296f17ee9c
Juan <juandeveloper.code@gmail.com>
2025-04-11 02:14:25 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ['./*.html', './js/**/*.js'],
+    theme: {
+        extend: {
+            fontFamily: {
+                akira: ['Akira', 'sans-serif'], 
+            },
+        },
+    },
+    plugins: [],
+};
